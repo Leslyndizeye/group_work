434f2c5e6718a79955ea0b92684c6ebb88a1c0c5
leslyndiz <139573491+Leslyndizeye@users.noreply.github.com>
2024-03-31 10:30:01 +0200

    lesly.sh

--- simple_todoList_application/lesly.sh
+++ simple_todoList_application/lesly.sh
@@ -6,17 +6,14 @@ docker login
 # Build Docker image
 sudo docker build -t vite-react-app:latest .
 
-# Run Docker container 
-# docker run -p 8080:8080 vite-react-app:latest
-
-# Tag Docker image
-docker tag vite-react-app:latest lscblack/group_assignment-todo-list:latest
+# Tag Docker image with the desired repository and tag
+docker tag vite-react-app:latest leslyndizeye/todolist_web_application:latest
 
 # Push Docker image to Docker Hub
-sudo docker push lscblack/group_assignment-todo-list:latest
+sudo docker push leslyndizeye/todolist_web_application:latest
 
 # Pull Docker image from Docker Hub
-docker pull lscblack/group_assignment-todo-list:latest
+docker pull leslyndizeye/todolist_web_application:latest
 
 # Run Docker container with pulled image
-docker run -p 8080:8080 lscblack/group_assignment-todo-list:latest
+docker run -p 8080:8080 leslyndizeye/todolist_web_application:latest
