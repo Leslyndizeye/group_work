61441997f23f15fd7c58283898017779050e2124
leslyndiz <139573491+Leslyndizeye@users.noreply.github.com>
2024-04-09 14:09:58 +0200

    ndizeye.sh

--- simple_todoList_application/ndizeye.sh
+++ simple_todoList_application/ndizeye.sh
@@ -1,19 +1,13 @@
-#!/bin/bash
 
-# Docker login
+
 docker login
 
-# Build Docker image
 sudo docker build -t vite-react-app:latest .
 
-# Tag Docker image with the desired repository and tag
 docker tag vite-react-app:latest leslyndizeye/todolist_web_application:latest
 
-# Push Docker image to Docker Hub
 sudo docker push leslyndizeye/todolist_web_application:latest
 
-# Pull Docker image from Docker Hub
 docker pull leslyndizeye/todolist_web_application:latest
 
-# Run Docker container with pulled image
 docker run -p 8080:8080 leslyndizeye/todolist_web_application:latest
